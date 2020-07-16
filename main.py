0ec3dd8c4b9afd60fae75caff24f035c24e26d32
Yoshikiyo Kosei <yoshikiyo.kosei.981@s.kyushu-u.ac.jp>
2020-07-16 15:49:41 +0900

    追加反応アップロード機能をあらかた完成させる

--- main.py
+++ main.py
@@ -4,32 +4,41 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
-
+#メイン画面
 @app.route('/main/')
 def main():
     return render_template("main.html")
-
+#smilesを送信したとき
 @app.route("/main/smiles")
 def returnSmiles():
     import re
     get = request.args.get("smiles","")
     out = get.translate(str.maketrans({"~":r"#"})) #送られてきたsmilesの~を#に翻訳
     out = re.sub("H","",out) #送られてきたsmilesのHを削除
+    getfilename = request.args.get("additionlFilename","")
 
     import search
     results = search.searchReaction(out)
+    try:
+        results = results + search.searchAdditionalReaction(out,getfilename)
+    except:
+        pass
 
     return render_template("list.html", results=results)
-
+#反応を選択したとき
 @app.route("/main/detail")
 def reactionDetail():
     getid = request.args.get("id","")
     getsmiles = request.args.get("smiles","")
     getsmiles = getsmiles.translate(str.maketrans({"~":r"#"}))
-    
-    import sql
-    reaction = sql.sqlSELECT("SELECT * FROM reaction WHERE id = %s" % str(getid))
+    getfilename = request.args.get("additionlFilename","")
 
+    if int(getid) < 10000:
+        import sql
+        reaction = sql.sqlSELECT("SELECT * FROM reaction WHERE id = %s" % str(getid))
+    else:
+        import search
+        reaction = search.getAdditionalReaction(int(getid),getfilename)
     #入力構造式のsvgを作成
     import structure
     structure.createImageHighlight(getsmiles, reaction[0]["site"])
@@ -55,15 +64,20 @@ def reactionDetail():
             structure.createImage(y)
 
     return render_template("detail.html", getid=getid, prelist=prelist, getsmiles=getsmiles, reaction=reaction[0], num=0)
-
+#反応の別パターンを選択したとき
 @app.route("/main/detail_celect")
 def reactionDetailCelect():
     getid = request.args.get("id","")
     getnum = request.args.get("num","")
     getsmiles = request.args.get("smiles","")
     getsmiles = getsmiles.translate(str.maketrans({"~":r"#"}))
-    import sql
-    reaction = sql.sqlSELECT("SELECT * FROM reaction WHERE id = %s" % str(getid))
+    getfilename = request.args.get("additionlFilename","")
+    if int(getid) < 10000:
+        import sql
+        reaction = sql.sqlSELECT("SELECT * FROM reaction WHERE id = %s" % str(getid))
+    else:
+        import search
+        reaction = search.getAdditionalReaction(int(getid),getfilename)
     #smartsによる変換の実行
     from rdkit import Chem
     from rdkit.Chem import AllChem
@@ -82,14 +96,26 @@ def reactionDetailCelect():
     prelist = get_unique_list(prelist)
 
     return render_template("detail.html", getid=getid, prelist=prelist, getsmiles=getsmiles, reaction=reaction[0], num=int(getnum))
-
+#設定から反応ファイルをアップロードしたとき
 @app.route("/main/upload", methods=["POST"])
 def upload_additionalData():
+    import os
+    import chardet
     if "file" not in request.files:
-        return "ファイルが正しく選択されていません"
+        return "エラー：ファイルが正しく選択されていません"
     fs = request.files["file"]
     filename = fs.filename
-
+    if not filename.endswith(".csv"):
+        return "エラー：ファイルがcsv形式ではありません"
+    saveaddress = "./static/csv/"
+    fs.save(os.path.join(saveaddress, filename)) #保存
+    #Shift_JISならutf-8に変換
+    with open(saveaddress+filename,"rb") as f:
+        s = f.read()
+        e = chardet.detect(s)
+        print(e["encoding"])
+        if e["encoding"] != "ascii" and e["encoding"] != "utf-8":
+            return "エラー：エンコード形式をutf-8にして下さい"
     return render_template("main.html", filename=filename)
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -19,15 +19,33 @@ def searchReaction(smiles):
 
     return hitData
 
-def searchAdditionalReaction(smiles, fileName):
+def searchAdditionalReaction(smiles, filename):
     from rdkit import Chem
     from rdkit.Chem import AllChem
     import csv
     mol = Chem.MolFromSmiles(smiles)
-    data = []
-    with open("/static/csv/"+fileName) as f:
+    reactionData = []
+    with open("/static/csv/"+filename) as f:
         reader = csv.reader(f)
         for row in reader:
-            data.append(row)
-    print(type(data))
-    print(data)
+            reactionData.append(row)
+    hitId = []
+    for data in reactionData:
+        smarts = Chem.MolFromSmarts(data["site"])
+        if(mol.HasSubstructMatch(smarts)):
+            hitId.append(data["id"])
+    hitData = []
+    for n in hitId:
+        hitData.append(reactionData[n-1])
+    return hitData
+
+def getAdditionalReaction(id, filename):
+    import csv
+    reaction = []
+    with open("/static/csv/"+filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == id:
+                reaction = row
+                break
+    return reaction
